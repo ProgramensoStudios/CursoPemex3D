e8ba32dca905a688ee6027462d1a233830c29259
Karina <kary.velle@gmail.com>
2025-09-05 16:34:37 -0600

    Agregando detalles del audio para el audio anager

---
+++ CursoPemex/Assets/Scripts/AudioScripts/PilarSound.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PilarSound : MonoBehaviour
+{
+    public AudioManager audioManager;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
